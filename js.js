874d90dadd261a4ca27779887aca7d6ce4686e84
Sylwester Biega <biegson11@gmail.com>
2021-10-20 12:50:49 +0200

    initial commit

---
+++ js.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+    const toggle = document.querySelector('.toggle')
+    const section = document.querySelector('section')
+    toggle.classList.toggle('active')
+    section.classList.toggle('active')
+}
